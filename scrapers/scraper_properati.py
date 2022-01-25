9729d752d7cb5ee372affe9de08326370d5702fc
Miguel Gómez <79323854+Murcicrum@users.noreply.github.com>
2022-01-24 21:01:03 -0300

    Update scraper_properati.py
    Más estable

--- scrapers/scraper_properati.py
+++ scrapers/scraper_properati.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import os, sys
 import time
 from numpy.random import random
@@ -19,35 +18,43 @@ def parse_url( url:str ):
     '''
     Dado un url devuelve objeto parseado de BeautifulSoup
     '''
-    try:
-        response = requests.get( url )
-    except:
-        print('***Error en conexion a', url)
-        return None
-        
-    if response.status_code != 200:
-        print('***Status code',response.status_code,'\tError en request', url)
-        return None
+    n_attempts=5
+    for _ in range(n_attempts):    
+        try:
+            response = requests.get( url )
+            
+            if response.status_code != 200:
+                print('***Error en request. Status code',response.status_code, url)
+                continue
+            
+            return bs( response.content, features="lxml" )
+            
+        except:
+            print('***Error en conexión a', url)
+                    
+    return None
     
-    return bs( response.content, features="lxml" )
-
-
-###DESCARGAR HTML Y VER EL RESULTADO
+    
 def get_links(url_search:str) -> list:
     '''
     Devuelve lista con todos los links a publicaciones de alquileres, dada una
-    url de busqueda con el formato que especifica numero de pagina
+    url de búsqueda con el formato que especifica numero de página
     '''
     URL_PROPERATI='https://www.properati.com.ar'
     urls = []
 
     soup = parse_url( url_search )
-    if not soup: return
+    if not soup: 
+        return []
     
-    tags = soup.find_all( name='div', attrs={'class':'StyledCardInfo-sc-n9541a-2'} )
+    tags = soup.find_all( name='a', attrs={'target':'_blank'} )
+
     for t in tags:
-        url_pub = URL_PROPERATI + t.find(name='a').attrs['href']
-        urls.append( url_pub )
+        attr_href = t['href']
+        url_pub = URL_PROPERATI + attr_href
+        
+        if attr_href.startswith('/detalle') and url_pub not in urls:
+            urls.append( url_pub )
 
 
     return urls
@@ -57,7 +64,7 @@ def get_links(url_search:str) -> list:
 def find_value(keys: str, dic: dict):
   '''
   Dado una secuencia de keys anidadas en el dict
-  Si encuentra cada una de las keys devuelve el valor, buscando     recursivamente
+  Si encuentra cada una de las keys devuelve el valor, buscando recursivamente
   '''
   list_keys = keys.split('.')
   if list_keys[0] in dic:
@@ -84,15 +91,14 @@ def get_data(urls: list) -> list:
     '''
 
     data_list = []      
-
-#cada key es la key con la que quedar�� registrada cada valor que querramos extraer del json
+#cada key es la key con la que quedarš¢ registrada cada valor que querramos extraer del json
 #cada value es un string con la secuencia de keys a recorrer en el json para extraer el valor deseado
     to_extract = {'ub_calle': 'address.street',
                   'ub_lat': 'geo_point.lat',
                   'ub_lon': 'geo_point.lon',
                   'pr_moneda': 'price.currency',
                   'pr_valor': 'price.amount',
-                  'pr_expen': 'maintenance_fees.price.amount'
+                  'pr_expen': 'maintenance_fees.price.amount',
                   'nu_ambs': 'floor_plan.rooms',
                   'nu_habs': 'floor_plan.bedrooms',
                   'sp_des': 'surface.total',
@@ -100,7 +106,7 @@ def get_data(urls: list) -> list:
                   'fe_pub': 'published_on'}
     
     for i, url in enumerate(urls):
-#Chequeo si el request sale bien y la publicación está disponible    
+#Chequeo si el request sale bien y la publicaciÃ³n estÃ¡ disponible    
         data = {}
         soup = parse_url( url )
         
@@ -111,12 +117,13 @@ def get_data(urls: list) -> list:
             for r, s in to_extract.items():
                 data[r] = find_value( s, d_props )
             data['url']=url
+            
+#            print(i, data)
+            
             data_list.append(data)                  
             
         except Exception as e:
-            print(e, url)
-                    
-
+            print('Exception: ',i, e, url)
 
     return data_list
     
@@ -131,7 +138,7 @@ def save_data(dicts:list, filepath) -> None:
         mode='a'
     else:               
         mode='x' 
-        print(f'No se encontro {filepath}, se creara a continuacion.')
+        print(f'No se encontró {filepath}, se creará a continuación.')
     
     with open(filepath, mode) as f:
         writer = csv.DictWriter(f, fieldnames=dicts[0].keys())
@@ -151,21 +158,21 @@ def save_data(dicts:list, filepath) -> None:
 def scrap(url, filename, ni=1, nf=1000):
 
     for n in range(ni,nf):
-        print(f'\tPagina {n}')
+        print(f'\tPágina {n}')
         url_page = url + str(n)
 
         print(f'\t\tSacando links')
         links = get_links(url_page)
 
         if len(links)==0:
-            print('Se alcanzo la ultima pagina.')
+            print('Se alcanzó la última página.')
             break
 
         print(f'\t\tSacando data')  
-        data = get_data(links)  #TARDA 1'20'' POR PAGINA
+        data = get_data(links)  #TARDA 6' POR PÁGINA!!!
         
         print(f'\t\tGuardando data')
-        save_data( data, filename )
+        save_data( data, filename ) 
         
 
 
@@ -178,7 +185,7 @@ if __name__=='__main__':
     URL_SEARCH_CASAS = 'https://www.properati.com.ar/s/capital-federal/casa/alquiler?sort=published_on_desc&page='
     URL_SEARCH_DEPTOS = 'https://www.properati.com.ar/s/capital-federal/departamento/alquiler?sort=published_on_desc&page='
     
-
+   
     search = {'phs': URL_SEARCH_PHS,
               'casas': URL_SEARCH_CASAS,
               'deptos': URL_SEARCH_DEPTOS }
@@ -186,7 +193,7 @@ if __name__=='__main__':
 
     for tipo, url in search.items():
         filepath = DIRECTORY + '_'.join([TODAY,tipo,'properati.csv'])
-        print(f'Escrapeando {tipo}.\nLos datos se guardaran en {filepath}')
-        scrap(url, filepath, ni=204)
+        print(f'Escrapeando {tipo}.\nLos datos se guardarán en {filepath}')
+        scrap(url, filepath)
         
     print('Todo OK :)')
